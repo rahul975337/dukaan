4514862dc3101174e4d4d893bbe735a9fbf8b637
Rahul Tilwani <tilwani03@gmail.com>
2023-03-21 20:05:47 +0530

    colors correction and input focus

--- src/components/Form/Seach.js
+++ src/components/Form/Seach.js
@@ -2,14 +2,17 @@ import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 export default function Search() {
   const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <div className={`search ${value ? "focused" : ""}`}>
+    <div className={`search ${isFocused ? "focused" : ""}`}>
       <input
         type="text"
         value={value}
         placeholder="Enter a word"
         onChange={(e) => setValue(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
       {value && (
         <AiOutlineClose className="close" onClick={() => setValue("")} />
