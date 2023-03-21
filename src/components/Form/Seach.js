1e4d39a5c07ee644424fa7591c517a9cad48b9fd
Rahul Tilwani <tilwani03@gmail.com>
2023-03-21 19:35:18 +0530

    input focus color change

--- src/components/Form/Seach.js
+++ src/components/Form/Seach.js
@@ -1,13 +1,11 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 export default function Search() {
-  const inputElt = useRef(null);
   const [value, setValue] = useState("");
 
   return (
-    <div className="search">
+    <div className={`search ${value ? "focused" : ""}`}>
       <input
-        ref={inputElt}
         type="text"
         value={value}
         placeholder="Enter a word"
